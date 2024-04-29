3a86050d191a564a26333cc99b8dedc602ec8e9a
User <User@USER-PC39>
2024-04-29 19:24:13 +0300

    Добавьте файлы проекта.

---
+++ Modul.py
@@ -0,0 +1,38 @@
+﻿def loe_failist(fail:str)->list:
+    """Loetakse informatsioon failist ja tagastatakse info nagu jarjend
+    :param str fail: faili nimetus
+    .rtype: list
+    """
+    f=open(fail,'r',encoding="utf-8")
+    jarjend=[]
+    for rida in f:
+        jarjend.append(rida.strip())
+    f.close()
+    return jarjend
+def tolkinine(sona:str,sonad:list,tolked:list)->str: #sonad-rus, tolked-est
+    if sona in sonad:
+        indeks=sonad.index(sona)
+        return tolked[indeks]
+    elif sona in tolked:
+        indeks=tolked.index(sona)
+        return sonad[indeks]
+    else:
+        return "Sona "+sona+" puudub"
+def teksti_lisamine_failisse(fail:str,sona:str,sonad:list):
+    sonad.append(sona)
+    f=open(fail,'a',encoding="utf-8")
+    f.write(sona+'\n')
+    f.close()
+def muutmine(sona:str,sonad:list,tolked:list)->str:
+     if sona in sonad:
+        indeks=sonad.index(sona)
+        oige=input("Sisesta oige sona: ")
+        sonad[indeks]=oige
+        return "ok!"
+     elif sona in tolked:
+        indeks=tolked.index(sona)
+        oige=input("Sisesta oige sona: ")
+        tolked[indeks]=oige
+        return "ok!"
+     else:
+        return "Sona "+sona+" puudub" 
---
+++ Sonastik.py
@@ -0,0 +1,52 @@
+﻿from Modul import *
+from random import *
+
+rus=[] #vene sonad
+est=[] #eesti sonad
+rus=loe_failist("Rus.txt")
+est=loe_failist("Est.txt")
+print(rus)
+print(est)
+while True:
+    v=input("\n1-Tolkimine\n2-Sonade lisamine\n3-Muutmine\n4-Kontroll\n5-Lopp\n")
+    if v=="1":
+        print("Tolkimine")
+        sona=input("Sisesta sona tolkimiseks: ")
+        tolke=tolkinine(sona,rus,est)
+        print(tolke)
+    elif v=="2":
+        print("Sonade lisamine")
+        est_sona=input("Sisesta sona eesti keeles: ")
+        rus_sona=input("Sisesta sona vene keeles: ")
+        teksti_lisamine_failisse("Est.txt",est_sona,est)
+        teksti_lisamine_failisse("rus.txt",rus_sona,rus)
+    elif v=="3":  
+        print("Sonade muutmine")
+        sona=input("Sisesta sona muutmiseks: ")
+        muutm=muutmine(sona,rus,est)
+        print(muutm)
+    elif v=="4":  
+        print("Teadmiste kontroll")
+        k=int(input("Kui palju sonad? "))
+        q=0
+        for i in range (0, k):
+            h=randint(0,1)
+            t=randint(0,len(rus)-1)
+            if h==0:
+                sona=rus[t]
+                tolk=est[t]
+            else:
+                sona=est[t]
+                tolk=rus[t]
+            kusimus="kusimus "+str(i+1)+": mis tahendab "+sona+"? "    
+            vastus=input(kusimus)
+            if vastus==tolk:
+                q+=1
+                print(" Oige!")
+            else:
+                print(" Vale! Oige vastus:", tolk)
+        h=(q*100)/k
+        print("Teie hinne:", str(int(h))+"%")
+    else:
+        print("Koike head!")
+        break
